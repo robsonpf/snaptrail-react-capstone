c07793c689c2cf7ad5f0a914ba7c6b9c3a0d0101
Justin Chau <chausicle@gmail.com>
2018-05-03 00:35:03 -0700

    Move store priovider

--- src/App.js
+++ src/App.js
@@ -2,27 +2,21 @@ import React, { Component } from 'react';
 import TopNav from './components/TopNav';
 import SignUp from './components/Signup';
 import Login from './components/Login';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import store from './redux/store'
-
-const newStore = store()
 
 export default class App extends Component {
   render() {
+    console.log('in app');
     return (
-      <Provider
-        store={newStore}>
-        <Router>
-          <div className="App">
-            <TopNav />
-            <Switch>
-              <Route path="/login" component={Login}/>
-              <Route path="/signup" component={SignUp}/>
-            </Switch>
-          </div>
-        </Router>
-      </Provider>
+      <Router>
+        <div className="App">
+          <TopNav />
+          <Switch>
+            <Route path="/login" component={Login}/>
+            <Route path="/signup" component={SignUp}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootswatch/dist/materia/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from 'react-redux';
+import store from './redux/store'
+
+const newStore = store()
 
 ReactDOM.render(
-    <App />,
+  <Provider
+    store={newStore}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
