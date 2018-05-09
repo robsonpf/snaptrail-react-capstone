5d906d42ccd5d85b1bccbba3635e45e8938c5f24
Robson Farias <fariasrobson.r@gmail.com>
2018-05-08 18:10:57 -0700

    Add component to user search the post on filter

---
+++ src/components/FilterPosts.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { filterByLocation } from '../redux/actions/filter'
+
+class FilterPosts extends Component {
+  render () {
+    return (
+      <Form inline>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Label for="filter-field" className="mr-sm-2">
+            Filter by location:
+          </Label>
+          <Input type="text"
+                name="location"
+                id="filter-field"
+                onChange={e => this.props.filterByLocation(e.target.value)}
+            />
+        </FormGroup>
+      </Form>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators({ filterByLocation }, dispatch)
+
+export default connect(null, mapDispatchToProps)(FilterPosts)
