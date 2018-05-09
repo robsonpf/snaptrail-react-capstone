798ff02da581f3365f9b03913be2c489460b97a8
Robson Farias <fariasrobson.r@gmail.com>
2018-05-08 18:21:47 -0700

    Import the filter actios and switch the case and return the state and the payload

---
+++ src/redux/reducers/comments.js
@@ -0,0 +1,23 @@
+import {
+  FETCH_COMMENTS_SUCCESS,
+  FETCH_COMMENTS_FAILED,
+  CREATE_COMMENT_SUCCESS,
+  CREATE_COMMENT_FAILED
+} from '../actions/comments'
+
+const initialState = []
+
+export default (state = initialState, { type, payload }) =>{
+  switch(type) {
+    case FETCH_COMMENTS_SUCCESS:
+      return [...payload]
+    case FETCH_COMMENTS_FAILED:
+      return payload
+    case CREATE_COMMENT_SUCCESS:
+      return [...state, payload]
+    case CREATE_COMMENT_FAILED:
+      return payload
+    default:
+      return state
+  }
+}
