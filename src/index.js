b6af38c67771f17ca0a97c068de37c2db048740f
Robson Farias <fariasrobson.r@gmail.com>
2018-05-08 17:58:19 -0700

    Dispatch get all posts to the redux store

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootswatch/dist/materia/bootstrap.min.css'
+import 'semantic-ui-css/semantic.min.css';
 import { Provider } from 'react-redux';
 import { fetchToken } from './redux/actions/token'
+import { getAllPosts } from './redux/actions/posts'
 import store from './redux/store'
 
 const newStore = store()
+
+newStore.dispatch(getAllPosts())
 
 ReactDOM.render(
   <Provider
