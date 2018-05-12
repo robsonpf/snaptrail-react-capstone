fad92615ec332a4e90b0ce19b46a4e93761e6636
Robson Farias <fariasrobson.r@gmail.com>
2018-05-11 22:51:27 -0700

    Added Feed for comment on the post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,7 +9,11 @@ import {
   Row,
   Col
 } from 'reactstrap'
-import { Comment } from 'semantic-ui-react'
+import {
+  Comment,
+  Feed,
+  Icon
+} from 'semantic-ui-react'
 import FaComment from 'react-icons/lib/fa/comment'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -38,8 +42,9 @@ const Post = (props) => {
         <Card>
           <CardBody>
             <CardTitle>
-              {username}
+              {username} <br/>
             </CardTitle>
+            <Moment fromNow ago>{created_at}</Moment> ago
             <CardTitle>
               {location}
             </CardTitle>
@@ -54,20 +59,50 @@ const Post = (props) => {
             alt="Card image cap"
           />
         <CardBody>
-          {/* <CardTitle>
-            {location}
-          </CardTitle>
-          <CardText>
-            {description}
-          </CardText>
-            <hr /> */}
-          <Moment fromNow ago>{created_at}</Moment> ago | {' '}
+
+          {/* <Feed size='large'>
+            <Feed.Event>
+              <Feed.Label image='/assets/images/avatar/small/elliot.jpg' />
+              <Feed.Content>
+                <Feed.Summary>
+                  <Feed.User>Elliot Fu</Feed.User> added you as a friend
+                  <Feed.Date>1 Hour Ago</Feed.Date>
+                </Feed.Summary>
+                <Feed.Meta>
+                  <Feed.Like>
+                    <Icon name='like' />
+                    4 Likes</Feed.Like>
+                </Feed.Meta>
+              </Feed.Content>
+            </Feed.Event>
+          </Feed> */}
+
           <FaComment /> {' '} {props.comments.length} {' '}
           {props.comments.length !== 1 ? 'Comments' : 'Comment'}
           <AddComment postId={id}/>
           <ul className="mt-2">
             {props.comments.map(comment => (
-              <li key={comment.id}>{comment.comment}</li>
+              // <li key={comment.id}>
+                <Feed size='large' key={comment.id}>
+                  <Feed.Event>
+                    <Feed.Label image={comment.user.user_image} />
+                    <Feed.Content>
+                      <Feed.Summary>
+                        <Feed.User>{comment.user.username}</Feed.User>
+                      <Feed.Date><Moment fromNow ago>{comment.created_at}</Moment> ago</Feed.Date>
+                      </Feed.Summary>
+                      <Feed.Extra text>
+                        {comment.comment}
+                      </Feed.Extra>
+                      <Feed.Meta>
+                        {/* <Feed.Like>
+                          <Icon name='like' />
+                          4 Likes</Feed.Like> */}
+                      </Feed.Meta>
+                    </Feed.Content>
+                  </Feed.Event>
+                </Feed>
+              // </li>
             ))}
           </ul>
         </CardBody>
@@ -78,11 +113,6 @@ const Post = (props) => {
 };
 
 const mapStateToProps = (state, props) => {
-  // console.log("STATECOMMENTS",  state.comments);
-  // console.log(props.post.user_id);
-  // console.log(state.users);
-  // console.log(state.users.find(user => user.id === props.post.user_id));
-  console.log('props ', props);
   return {
     comments: state.comments.filter(comment => comment.post_id === props.post.id),
     // user: props.user
