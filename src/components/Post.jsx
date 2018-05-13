f83edc5fbdbc537c2e02be0349f8a4b75967f6ce
Justin Chau <chausicle@gmail.com>
2018-05-12 22:57:19 -0700

    Add like skeleton

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,6 +15,8 @@ import {
   Icon
 } from 'semantic-ui-react'
 import FaComment from 'react-icons/lib/fa/comment'
+import FaThumbsUp from 'react-icons/lib/fa/thumbs-up'
+import FaThumbsDown from 'react-icons/lib/fa/thumbs-down'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { createComment } from '../redux/actions/comments'
@@ -32,8 +34,6 @@ const Post = (props) => {
     user: { email, username, user_image }
   } = props.post
   console.log("email", email, "username", username, "user_image", user_image);
-  // let { username, user_image } = props.user
-  // console.log("PROPS.POST ====> ", props.post);
 
   console.log(props);
   return (
@@ -58,64 +58,64 @@ const Post = (props) => {
             src={image_url}
             alt="Card image cap"
           />
-        <CardBody>
+          <CardBody>
 
-          {/* <Feed size='large'>
-            <Feed.Event>
+            {/* <Feed size='large'>
+              <Feed.Event>
               <Feed.Label image='/assets/images/avatar/small/elliot.jpg' />
               <Feed.Content>
                 <Feed.Summary>
-                  <Feed.User>Elliot Fu</Feed.User> added you as a friend
-                  <Feed.Date>1 Hour Ago</Feed.Date>
+              <Feed.User>Elliot Fu</Feed.User> added you as a friend
+              <Feed.Date>1 Hour Ago</Feed.Date>
                 </Feed.Summary>
                 <Feed.Meta>
-                  <Feed.Like>
-                    <Icon name='like' />
-                    4 Likes</Feed.Like>
+              <Feed.Like>
+              <Icon name='like' />
+              4 Likes</Feed.Like>
                 </Feed.Meta>
               </Feed.Content>
-            </Feed.Event>
-          </Feed> */}
+              </Feed.Event>
+            </Feed> */}
 
-          <FaComment /> {' '} {props.comments.length} {' '}
-          {props.comments.length !== 1 ? 'Comments' : 'Comment'}
-          <AddComment postId={id}/>
-          <ul className="mt-2">
+            <FaComment className="text-primary"/>
+            {`  `} {props.comments.length} {`  `}
+            {props.comments.length !== 1 ? 'Comments' : 'Comment'}
+            {`  |  `}
+            <FaThumbsUp className="text-primary"/>
+            {`  `} {props.comments.length} {`  `}
+            {props.comments.length !== 1 ? 'Likes' : 'Like'}
+            <AddComment postId={id}/>
             {props.comments.map(comment => (
-              // <li key={comment.id}>
-                <Feed size='large' key={comment.id}>
-                  <Feed.Event>
-                    <Feed.Label image={comment.user.user_image} />
-                    <Feed.Content>
-                      <Feed.Summary>
-                        <Feed.User>{comment.user.username}</Feed.User>
+              <Feed size='large' key={comment.id}>
+                <Feed.Event>
+                  <Feed.Label image={comment.user.user_image} />
+                  <Feed.Content>
+                    <Feed.Summary>
+                      <Feed.User>{comment.user.username}</Feed.User>
                       <Feed.Date><Moment fromNow ago>{comment.created_at}</Moment> ago</Feed.Date>
-                      </Feed.Summary>
-                      <Feed.Extra text>
-                        {comment.comment}
-                      </Feed.Extra>
-                      <Feed.Meta>
-                        {/* <Feed.Like>
-                          <Icon name='like' />
-                          4 Likes</Feed.Like> */}
-                      </Feed.Meta>
-                    </Feed.Content>
-                  </Feed.Event>
-                </Feed>
-              // </li>
+                    </Feed.Summary>
+                    <Feed.Extra text>
+                      {comment.comment}
+                    </Feed.Extra>
+                    <Feed.Meta>
+                      {/* <Feed.Like>
+                        <Icon name='like' />
+                      4 Likes</Feed.Like> */}
+                    </Feed.Meta>
+                  </Feed.Content>
+                </Feed.Event>
+              </Feed>
             ))}
-          </ul>
-        </CardBody>
-      </Card>
-     </Col>
+          </CardBody>
+        </Card>
+      </Col>
     </Row>
   )
 };
 
 const mapStateToProps = (state, props) => {
   return {
-    comments: state.comments.filter(comment => comment.post_id === props.post.id),
-    // user: props.user
+    comments: state.comments.filter(comment => comment.post_id === props.post.id)
   }
 };
 
